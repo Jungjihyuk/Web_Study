0bfa29d65cf662f16a0d4544ee7981ebaf9eee2b
jeongjihyeok <33630505+Jungjihyuk@users.noreply.github.com>
2019-06-04 14:08:18 +0900

    Create models.py

---
+++ python_files/models.py
@@ -0,0 +1,15 @@
+from extension import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    # pets = db.relationship('Pet', backref='owner')
+
+    def __str__(self):
+        return "{}, {}".format(self.last_name, self.first_name)
+
+    def __repr__(self):
+        return "{}: {}".format(self.id, self.__str__())
